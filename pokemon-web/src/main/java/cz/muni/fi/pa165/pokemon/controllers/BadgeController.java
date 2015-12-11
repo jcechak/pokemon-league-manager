9a8298dabf6ffb85f8957405114be3ee24d3d960
aimemilos <396568@mail.muni.cz>
2015-12-11 19:51:21 +0100

    added stadiums

--- pokemon-web/src/main/java/cz/muni/fi/pa165/pokemon/controllers/BadgeController.java
+++ pokemon-web/src/main/java/cz/muni/fi/pa165/pokemon/controllers/BadgeController.java
@@ -1,7 +1,10 @@
 package cz.muni.fi.pa165.pokemon.controllers;
 
 import cz.muni.fi.pa165.pokemon.dto.BadgeDTO;
+import cz.muni.fi.pa165.pokemon.dto.StadiumDTO;
 import cz.muni.fi.pa165.pokemon.facade.BadgeFacade;
+import cz.muni.fi.pa165.pokemon.facade.StadiumFacade;
+import java.util.ArrayList;
 import java.util.Collection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -16,18 +19,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/menu")
 public class BadgeController {
     
-    /*@Autowired
+    @Autowired
     private BadgeFacade badgeFacade;
+    
+    @Autowired
+    private StadiumFacade stadiumFacade;
     
     public void setBadgeFacade(BadgeFacade badgeFacade) {
         this.badgeFacade = badgeFacade;
-    }*/
+    }
     
     @RequestMapping(value = "/badgeList")
     public String showList(Model model) {
+        Collection<BadgeDTO> badges = badgeFacade.getAllBadges();
+        Collection<StadiumDTO> stadiums = new ArrayList<StadiumDTO>();
+        for(BadgeDTO b:badges) {
+            System.out.println("DEBUG " + b);
+            stadiums.add(stadiumFacade.findById(b.getId()));
+        }
+        model.addAttribute("badges", badges);
+        model.addAttribute("stadiums", stadiums);
         
-        /*Collection<BadgeDTO> badges = badgeFacade.getAllBadges();
-        model.addAttribute("badges", badges);*/
+        
+        
         return "/menu/badgeList";
     }
 }
