be89e8776b4cb87b7d60a506316fbec5eb0c478b
aimemilos <396568@mail.muni.cz>
2015-12-11 15:36:38 +0100

    web initializer reimplemented

--- pokemon-web/src/main/java/cz/muni/fi/pa165/config/WebInitializer.java
+++ pokemon-web/src/main/java/cz/muni/fi/pa165/config/WebInitializer.java
@@ -1,29 +1,29 @@
 package cz.muni.fi.pa165.config;
 
-import javax.servlet.ServletContext;  
-import javax.servlet.ServletException;  
-import javax.servlet.ServletRegistration.Dynamic;  
-  
-import org.springframework.web.WebApplicationInitializer;  
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;  
-import org.springframework.web.servlet.DispatcherServlet;  
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 
 /**
  *
  * @author Milos Bartak
  */
-public class WebInitializer implements WebApplicationInitializer {
-    
+public class WebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer{
+
     @Override
-    public void onStartup(ServletContext servletContext) throws ServletException {        
-        AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();  
-        ctx.register(Config.class);  
-        ctx.setServletContext(servletContext);    
-        Dynamic servlet = servletContext.addServlet("dispatcher", new DispatcherServlet(ctx));  
-        servlet.addMapping("/");  
-        servlet.setLoadOnStartup(1);
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[]{Config.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
     }
     
+        
 }
 
