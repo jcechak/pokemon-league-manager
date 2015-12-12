e07b07b1b9921c837da14761f6f30baadb1a7fdd
aimemilos <396568@mail.muni.cz>
2015-12-12 13:07:52 +0100

    badge list added to frontend view

--- pokemon-web/src/main/java/cz/muni/fi/pa165/pokemon/controllers/BadgeController.java
+++ pokemon-web/src/main/java/cz/muni/fi/pa165/pokemon/controllers/BadgeController.java
@@ -6,6 +6,8 @@ import cz.muni.fi.pa165.pokemon.facade.BadgeFacade;
 import cz.muni.fi.pa165.pokemon.facade.StadiumFacade;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -32,13 +34,13 @@ public class BadgeController {
     @RequestMapping(value = "/badgeList")
     public String showList(Model model) {
         Collection<BadgeDTO> badges = badgeFacade.getAllBadges();
-        Collection<StadiumDTO> stadiums = new ArrayList<StadiumDTO>();
+        Map<Long, StadiumDTO> badgesAndStadiums = new HashMap<>();
         for(BadgeDTO b:badges) {
             System.out.println("DEBUG " + b);
-            stadiums.add(stadiumFacade.findById(b.getId()));
+            badgesAndStadiums.put(b.getId(), stadiumFacade.findById(b.getId()));
         }
         model.addAttribute("badges", badges);
-        model.addAttribute("stadiums", stadiums);
+        model.addAttribute("stadiumsMap", badgesAndStadiums);
         
         
         
