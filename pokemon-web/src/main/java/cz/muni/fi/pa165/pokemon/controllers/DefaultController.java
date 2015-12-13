5b7f007a4c2a28820b9da04cac37e1e80ab75658
aimemilos <396568@mail.muni.cz>
2015-12-13 14:11:33 +0100

    totally reimplemented because of login page

--- pokemon-web/src/main/java/cz/muni/fi/pa165/pokemon/controllers/DefaultController.java
+++ pokemon-web/src/main/java/cz/muni/fi/pa165/pokemon/controllers/DefaultController.java
@@ -11,11 +11,24 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * @author Milos Bartak
  */
 @Controller
+@RequestMapping("/")
 public class DefaultController {
     
+   @RequestMapping(value = "/login", method = RequestMethod.GET)
+   public String login(ModelMap map) {
+       System.out.println("default login controller called");
+       return "login";
+   }
+   
+   @RequestMapping(value = "/logout", method = RequestMethod.GET)
+   public String logout(ModelMap map) {
+       System.out.println("default logout controller called");
+       return "logout";
+   }
+
    @RequestMapping(value = "/", method = RequestMethod.GET)
    public String index(ModelMap map) {
-       return "index";
+       System.out.println("default controller called");
+       return "redirect:/menu/menu";
    }
-    
 }
