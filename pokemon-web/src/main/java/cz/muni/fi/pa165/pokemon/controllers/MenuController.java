44f6c77e6f184f676af654ef3441df92dce459a0
aimemilos <396568@mail.muni.cz>
2015-12-18 13:23:57 +0100

    username attribute added

--- pokemon-web/src/main/java/cz/muni/fi/pa165/pokemon/controllers/MenuController.java
+++ pokemon-web/src/main/java/cz/muni/fi/pa165/pokemon/controllers/MenuController.java
@@ -1,6 +1,10 @@
 package cz.muni.fi.pa165.pokemon.controllers;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -12,7 +16,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MenuController {
     
     @RequestMapping(value = "/menu")
-    public String showMenu() {
+    public String showMenu(ModelMap map) {
         return "/menu/menu";
+    }
+    
+    @ModelAttribute("userName")
+    public String userName() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String name = auth.getName();
+        return name;
     }
 }
